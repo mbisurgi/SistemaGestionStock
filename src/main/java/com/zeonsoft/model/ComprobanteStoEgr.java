e4428cf9740c01e2f0491dc2e30a54180b1b1bfd
mbisurgi <mbisurgi@bc-group.com.ar>
2016-08-16 13:46:58 -0300

    Pruebas

--- src/main/java/com/zeonsoft/model/ComprobanteStoEgr.java
+++ src/main/java/com/zeonsoft/model/ComprobanteStoEgr.java
@@ -1,5 +1,7 @@
 package com.zeonsoft.model;
 
+import com.zeonsoft.controller.SistemaGestionStock;
+
 import java.sql.Date;
 
 public class ComprobanteStoEgr extends Comprobante {
@@ -12,6 +14,18 @@ public class ComprobanteStoEgr extends Comprobante {
 
     @Override
     public void updateStock() {
+        for (ItemComprobante item: this.getItems()) {
+            Articulo art = SistemaGestionStock.getInstancia().buscarArticulo(item.getArticulo().getNroArticulo());
 
+            art.getStock().addItem(this.getFecha(), item.getCantidad(), item.getPrecio());
+
+            int cantidadArt = art.getStock().getCantidad();
+
+            int newCantidad = 0;
+
+            newCantidad = cantidadArt + item.getCantidad() * -1;
+
+            art.getStock().setCantidad(newCantidad);
+        }
     }
 }
--- src/main/java/com/zeonsoft/model/ComprobanteStoIng.java
+++ src/main/java/com/zeonsoft/model/ComprobanteStoIng.java
@@ -1,5 +1,7 @@
 package com.zeonsoft.model;
 
+import com.zeonsoft.controller.SistemaGestionStock;
+
 import java.sql.Date;
 
 public class ComprobanteStoIng extends Comprobante {
@@ -12,6 +14,18 @@ public class ComprobanteStoIng extends Comprobante {
 
     @Override
     public void updateStock() {
+        for (ItemComprobante item: this.getItems()) {
+            Articulo art = SistemaGestionStock.getInstancia().buscarArticulo(item.getArticulo().getNroArticulo());
 
+            art.getStock().addItem(this.getFecha(), item.getCantidad(), item.getPrecio());
+
+            int cantidadArt = art.getStock().getCantidad();
+
+            int newCantidad = 0;
+
+            newCantidad = cantidadArt + item.getCantidad();
+
+            art.getStock().setCantidad(newCantidad);
+        }
     }
 }
